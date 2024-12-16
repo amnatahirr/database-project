1e2a8ce4fa970c4e0935d014ea4c2363119add2c
AreejFatim124 <Sp23-BSE-041@cuilahore.edu.pk>
2024-12-16 22:36:00 +0500

    form formating

--- database project/server.js
+++ database project/server.js
@@ -107,7 +107,7 @@ app.get('/JobApplicationForm',(req,res)=>{
 
 connectDB();
 // Start the server
-const PORT = 5000;
+const PORT = 5002;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 
