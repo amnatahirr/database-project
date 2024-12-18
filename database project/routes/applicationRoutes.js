2dcdbbb27944f1ccd9c6e5b1b97d4e04b09ccc5a
ATAKAQAISER44 <sp23-bse-044@cuilahore.edu.pk>
2024-12-18 22:51:04 +0500

    comit

--- database project/routes/applicationRoutes.js
+++ database project/routes/applicationRoutes.js
@@ -22,8 +22,3 @@ module.exports = router;
 
 
 
-// cloudinary.config({
-//   cloud_name: process.env.CLOUD_NAME,
-//   api_key: process.env.CLOUD_API_KEY,
-//   api_secret: process.env.CLOUD_API_SECRET,
-// });
