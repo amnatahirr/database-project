83ac6eceb804158e7ce7d40b5903eb3fd1ee35de
AreejFatim124 <Sp23-BSE-041@cuilahore.edu.pk>
2024-12-18 14:02:30 +0500

    chal gaya he akhir

--- database project/routes/jobRoutes.js
+++ database project/routes/jobRoutes.js
@@ -13,7 +13,7 @@ const {authenticateAccessToken} = require("../middleware/auth");
 const router = express.Router();
 router.get("/viewJob", getAllJobs);
 router.post("/jobPostForm",authenticateAccessToken, postJob);
-router.get("/MyJobs", getMyJobs);
+router.get("/MyJobs",authenticateAccessToken, getMyJobs);
 router.put("/update/:id",authenticateAccessToken, updateJob);
 router.delete("/delete/:id",authenticateAccessToken, deleteJob);
 router.get("/:id", authenticateAccessToken, getSingleJob);
