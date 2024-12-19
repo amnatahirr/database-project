16d847376d86baf3ba6f67b9746161a70e9a39fd
ATAKAQAISER44 <sp23-bse-044@cuilahore.edu.pk>
2024-12-19 06:13:20 +0500

    apply now for  job

--- database project/models/application.js
+++ database project/models/application.js
@@ -3,46 +3,43 @@ const validator = require('validator');
 
 const applicationSchema = new mongoose.Schema({
   jobId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Job", // Reference to the Job model
-    required: true,
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Job", // Reference to the Job model
+      required: true,
   },
   name: {
-    type: String,
-    required: [true, "Please enter your Name!"],
-    minLength: [3, "Name must contain at least 3 Characters!"],
-    maxLength: [30, "Name cannot exceed 30 Characters!"],
+      type: String,
+      required: [true, "Please enter your Name!"],
+      minLength: [3, "Name must contain at least 3 Characters!"],
+      maxLength: [30, "Name cannot exceed 30 Characters!"],
   },
   email: {
-    type: String,
-    required: [true, "Please enter your Email!"],
-    validate: [validator.isEmail, "Please provide a valid Email!"],
+      type: String,
+      required: [true, "Please enter your Email!"],
+      validate: [validator.isEmail, "Please provide a valid Email!"],
   },
   coverLetter: {
-    type: String,
-    required: [true, "Please provide a cover letter!"],
+      type: String,
+      required: [true, "Please provide a cover letter!"],
   },
   phone: {
-    type: Number,
-    required: [true, "Please enter your Phone Number!"],
+      type: String, // Changed from Number to String
+      required: [true, "Please enter your Phone Number!"],
   },
   address: {
-    type: String,
-    required: [true, "Please enter your Address!"],
+      type: String,
+      required: [true, "Please enter your Address!"],
   },
   applicantID: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",  // User model
-    required: true,
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",  // User model
+      required: true,
   },
   employerID: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",  // User model
-    required: true,
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",  // User model
+      required: true,
   },
 }, { timestamps: true });
 
-// Avoid model overwrite error
-const Application = mongoose.models.Application || mongoose.model("Application", applicationSchema);
-
-module.exports = Application;
+module.exports = mongoose.models.Application || mongoose.model("Application", applicationSchema);
