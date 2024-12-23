af6b897dd8333e159a70204a3ceadbd6359d31b4
ATAKAQAISER44 <sp23-bse-044@cuilahore.edu.pk>
2024-12-23 13:18:47 +0500

    commit

--- database project/models/application.js
+++ database project/models/application.js
@@ -1,48 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require('validator');
 
-// const applicationSchema = new mongoose.Schema({
-//   jobId: {
-//       type: mongoose.Schema.Types.ObjectId,
-//       ref: "Job", // Reference to the Job model
-//       required: true,
-//   },
-//   name: {
-//       type: String,
-//       required: [true, "Please enter your Name!"],
-//       minLength: [3, "Name must contain at least 3 Characters!"],
-//       maxLength: [30, "Name cannot exceed 30 Characters!"],
-//   },
-//   email: {
-//       type: String,
-//       required: [true, "Please enter your Email!"],
-//       validate: [validator.isEmail, "Please provide a valid Email!"],
-//   },
-//   coverLetter: {
-//       type: String,
-//       required: [true, "Please provide a cover letter!"],
-//   },
-//   phone: {
-//       type: String, // Changed from Number to String
-//       required: [true, "Please enter your Phone Number!"],
-//   },
-//   address: {
-//       type: String,
-//       required: [true, "Please enter your Address!"],
-//   },
-//   applicantID: {
-//       type: mongoose.Schema.Types.ObjectId,
-//       ref: "User",  // User model
-//       required: true,
-//   },
-//   employerID: {
-//       type: mongoose.Schema.Types.ObjectId,
-//       ref: "User",  // User model
-//       required: true,
-//   },
-// }, { timestamps: true });
-
-// module.exports = mongoose.models.Application || mongoose.model("Application", applicationSchema);
 
 const applicationSchema = new mongoose.Schema({
     jobId: {
